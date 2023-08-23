12c3a16afd1b9bddb1d17f0952f4c8b6b998b0e3
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-08-24 09:35:56 +1000

    Handle info error when id not found

--- src/main/kotlin/app/urbanflo/urbanflosumoserver/config/WebConfig.kt
+++ src/main/kotlin/app/urbanflo/urbanflosumoserver/config/WebConfig.kt
@@ -1,7 +1,12 @@
 package app.urbanflo.urbanflosumoserver.config
 
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -14,5 +19,13 @@ class WebConfig: WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**").allowedOrigins(frontendUrl)
+    }
+
+    override fun configureMessageConverters(converters: MutableList<HttpMessageConverter<*>>) {
+        // Always write date/time in ISO format instead of unix timestamp
+        val objectMapper = jacksonObjectMapper()
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        objectMapper.registerModules(JavaTimeModule())
+        converters.add(MappingJackson2HttpMessageConverter(objectMapper))
     }
 }
