0ba60b2a77ab6117f277c8c248e355a7697a6e2b
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-10-24 10:40:24 +1100

    Document netconvert and simulation classes

--- src/main/kotlin/app/urbanflo/urbanflosumoserver/netconvert/Netconvert.kt
+++ src/main/kotlin/app/urbanflo/urbanflosumoserver/netconvert/Netconvert.kt
@@ -4,6 +4,17 @@ import app.urbanflo.urbanflosumoserver.simulation.SimulationId
 import java.io.IOException
 import java.nio.file.Path
 
+/**
+ * Converts the node, edge and connection files to a network XML file using `netconvert`.
+ *
+ * @param simulationId Simulation ID
+ * @param simulationDir path to root directory where all simulation files are stored
+ * @param nodPath path to nodes XMl file
+ * @param edgPath path to edges XMl file
+ * @param conPath path to connections XMl file
+ * @return A path to the network XML file
+ * @throws NetconvertException if `netconvert` returns a non-zero exit status
+ */
 fun runNetconvert(simulationId: SimulationId, simulationDir: Path, nodPath: Path, edgPath: Path, conPath: Path): Path {
     val netPath = simulationDir.resolve("$simulationId.net.xml").normalize().toAbsolutePath()
     val netconvertCmd =
@@ -20,7 +31,9 @@ fun runNetconvert(simulationId: SimulationId, simulationDir: Path, nodPath: Path
         .redirectError(ProcessBuilder.Redirect.PIPE)
         .start()
     val statusCode = process.waitFor()
-    if (statusCode != 0) {
+    if (statusCode == 0) {
+        return netPath
+    } else {
         val stdout = try {
             process.inputStream.bufferedReader().readText()
         } catch (e: IOException) {
@@ -32,7 +45,5 @@ fun runNetconvert(simulationId: SimulationId, simulationDir: Path, nodPath: Path
             ""
         }
         throw NetconvertException("netconvert exited with status code $statusCode\nstdout:\n$stdout\nstderr:\n$stderr")
-    } else {
-        return netPath
     }
 }
--- src/main/kotlin/app/urbanflo/urbanflosumoserver/netconvert/NetconvertException.kt
+++ src/main/kotlin/app/urbanflo/urbanflosumoserver/netconvert/NetconvertException.kt
@@ -1,3 +1,6 @@
 package app.urbanflo.urbanflosumoserver.netconvert
 
+/**
+ * Exception for errors in [runNetconvert], which is when `netconvert` returns a non-zero exit status.
+ */
 class NetconvertException(message: String) : Exception(message)
