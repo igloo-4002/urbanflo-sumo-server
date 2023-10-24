b1ede05ee56c85fc18bbec43eea431425ee223fe
Akbar Fadiansyah <afad0006@student.monash.edu>
2023-10-24 12:14:51 +1100

    Document storage classes

--- src/main/kotlin/app/urbanflo/urbanflosumoserver/storage/StorageProperties.kt
+++ src/main/kotlin/app/urbanflo/urbanflosumoserver/storage/StorageProperties.kt
@@ -3,6 +3,9 @@ package app.urbanflo.urbanflosumoserver.storage
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
+/**
+ * Component which stores the root directory for all simulation files.
+ */
 @Component
 class StorageProperties {
     @Value("\${urbanflo.storage.location:uploads}")
